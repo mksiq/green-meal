c6aa1544faa6f99c4e78740e137eb63e6bcc40da
mksiq <msiqueira@myseneca.ca>
2020-12-04 10:48:00 -0500

    Button to add to cart on main page works now

--- controllers/general.js
+++ controllers/general.js
@@ -5,9 +5,13 @@ router.get("/", (req, res) => {
     const productModel = require("../models/products.js");
     productModel.find().lean().exec()
         .then(products => {
+            let user = req.session.user;
             if (products) {
                 res.render('general/home',
-                    { data: products });
+                    { 
+                        data: products,
+                        user: user
+                    });
             } else {
                 console.log("Error loading database");
             }
--- controllers/on-the-menu.js
+++ controllers/on-the-menu.js
@@ -11,7 +11,7 @@ router.get("/", (req, res) => {
                 let salads = products.filter((product) => { if (product.category.toLowerCase() == "salads") return product });
                 let sandwiches = products.filter((product) => { if (product.category.toLowerCase() == "sandwiches") return product });
                 let soups = products.filter((product) => { if (product.category.toLowerCase() == "soups") return product });
-            
+                let user = req.session.user;
                 res.render('general/on-the-menu',
                     {
                         //data is for the details modals
@@ -21,6 +21,7 @@ router.get("/", (req, res) => {
                         saladsData: salads,
                         sandwichesData: sandwiches,
                         soupsData: soups,
+                        user: user
                     });
             } else {
                 console.log("Error loading database");
